744586738c42c1aa142b230c4b5b00f7e5864f36
kimi <kimi@62fcdf77-6916-4821-beed-8fe5a9fc44fa>
2016-01-12 03:29:59 +0000

    ST874 "(Due to 1/13)SAAS: 請看附檔圖:當applicant收到一個Offer時,appplicant會先看到"Offer Screen 1"的訊息,他點了accept或decline之後,才會看到"Offer Screen 2"的畫面(這頁的欄位名/和button字請照圖寫的去修改) 這個"Offer Screen 2" Terms of Appointment黃框框內的字需要可以編輯, 每個Role所看到的Terms of Appointment每年都會改, 所以程式不能寫死, 請在Set up positions>Roles那邊增加一個叫做Terms of Appointment的欄位可以編輯文字(參考目前system message的文字編輯做法)"

--- db/migrate/20160112032113_add_field_appointment_to_positions.rb
+++ db/migrate/20160112032113_add_field_appointment_to_positions.rb
@@ -1,6 +1,6 @@
 class AddFieldAppointmentToPositions < ActiveRecord::Migration
   def change
-    add_column :positions, :appointment, :string, limit: 1024
+    add_column :positions, :offer_appointment, :string, limit: 1024, default: "Do you really want to confirm/decline this job offer?\nOnce you confirm a job, you will not be able to accept other jobs."
   end
 end
 
