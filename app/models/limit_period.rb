0d63b00baadd61bba43ea1d9486f9e052fbe362d
bill <bill@62fcdf77-6916-4821-beed-8fe5a9fc44fa>
2017-04-15 09:03:35 +0000

    1. ST1031 "The time function is impaired in our Pre-Assignment portal for SAAS in multiple modules. One issue we’ve noticed is that times are saved and function as an hour later than the time we put in. For example, when I set the manage applicants time allowed to 9:00am, after I hit update, the system saves it as 10am (screenshot attached). This is also true of gate times. For example, we set the application to open at 1pm on 4/4/2017, but it wouldn’t open until 2pm; we had to manually set it to open at 12pm in order for it to actually be open/live for applicants at 1pm.
    We’ve also noticed some discrepancies between the gate times we set and what is displayed in the student user view. Times we set as PM show up as AM in the student view (screen shot attached); we input 1pm, but it shows up as 1AM and doesn’t work until 2pm. -> Production -> Pre-Assignment -> 1. Manage Applicants -> time slot -> 設定的時間會比實際的時間晚一小時才work，儲存後也會晚一小時，例如: start time設定04/14/2017 09:00 AM，儲存後會變成04/14/2017 10:00 AM 2. 後台設定04/04/2017 1: 00PM， Applicants看到的卻是04/04/2017 1: 00 AM

--- app/models/limit_period.rb
+++ app/models/limit_period.rb
@@ -19,11 +19,13 @@ class LimitPeriod < ActiveRecord::Base
   end
 
   def t_start_string
-    return self.t_start.in_time_zone("Pacific Time (US & Canada)").strftime("%m/%d/%Y %I:%M %p")
+    # return self.t_start.in_time_zone("Pacific Time (US & Canada)").strftime("%m/%d/%Y %I:%M %p")
+    return (self.t_start - 8.hours).strftime("%m/%d/%Y %I:%M %p")
   end
 
   def t_end_string
-    return self.t_end.in_time_zone("Pacific Time (US & Canada)").strftime("%m/%d/%Y %I:%M %p")
+    # return self.t_end.in_time_zone("Pacific Time (US & Canada)").strftime("%m/%d/%Y %I:%M %p")
+    return (self.t_end - 8.hours).strftime("%m/%d/%Y %I:%M %p")
   end
 
 end
